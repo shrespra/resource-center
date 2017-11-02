b033525f605b438f463b08aef02c4e8a9c43755e
shrespra <pranesh.shrestha1@gmail.com>
2017-11-01 18:56:29 -0600

    Added icon and button in app.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-
-//import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import HardwareVideogameAsset from 'material-ui/svg-icons/hardware/videogame-asset';
+import {red500, yellow500, blue500} from 'material-ui/styles/colors';
+import iconStyles from './index.css';
+import Avatar from 'material-ui/Avatar';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const style = {margin: 50};
+
 injectTapEventPlugin()
 
 class App extends Component {
@@ -19,10 +34,11 @@ class App extends Component {
     return (
       <div className="App">
       <AppBar
-        title="Title"
+        title="CS 476 Project"
         iconClassNameRight="muidocs-icon-navigation-expand-more"
         onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
       />
+
       <Drawer
         open={this.state.open}
         docked={false}
@@ -31,6 +47,30 @@ class App extends Component {
           <MenuItem>Menu Item</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
         </Drawer>
+
+        <div>
+          <Avatar
+          icon={<HardwareVideogameAsset />}
+          //color={orange200}
+          //backgroundColor={pink400}
+          size={150}
+          style={style}
+          />
+        </div>
+
+        <div>
+        <RaisedButton
+          label="Login With Discord"
+          width={'300px'}
+          primary={true}
+          style={iconStyles}
+          />
+        </div>
+
+          
+
+
+
 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+const muiTheme = getMuiTheme({
+
+//This is to change color
+  //palette: {
+    //primary1Color: '#21412a',
+  //},
+
+  AppBar: {
+    height: '500',
+  },
+//RaisedButton: {
+  //height: 100,
+//},
+
+});
+
+ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
